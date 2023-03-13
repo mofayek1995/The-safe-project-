e8617db65ef0f8667e1462b9e7953ce276f0914a
mohamed fayek <90655788+mofayek1995@users.noreply.github.com>
2023-03-13 08:06:35 +0200

    Add files via upload

---
+++ LCD_config.h
@@ -0,0 +1,12 @@
+/*
+ * LCD_config.h
+ *
+ * Created: 2/23/2023 7:55:06 PM
+ *  Author: Mohamed fayek 
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+#define eight_bits_mode
+#endif /* LCD_CONFIG_H_ */
